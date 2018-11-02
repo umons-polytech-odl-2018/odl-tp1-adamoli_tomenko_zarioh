06ffadbd2ea15a1f055611d24c95cc0a128f59b1
PTom8 <171567@umons.ac.be>
2018-11-02 16:21:20 +0100

    mehdi aime la tajine au chocolat

--- src/main/java/exercise3/Exercise3.java
+++ src/main/java/exercise3/Exercise3.java
@@ -3,27 +3,31 @@ package exercise3;
 public class Exercise3 {
 	static ConvexPolygon buildConvexPolygon(Point[] vertices) {
 		ConvexPolygon convexPolygon = new ConvexPolygon(vertices);
-		System.out.println("perimetre polygone = "+ convexPolygon.perimeter() + " aire = " + convexPolygon.area());
+		System.out.println("polygone : perimetre = "+ convexPolygon.perimeter() + " aire = " + convexPolygon.area());
 		return null;
 	}
 
 	static ConvexPolygon buildRectangle(Point upperLeft, Point lowerRight) {
 		Rectangle rectangle = new Rectangle(upperLeft, lowerRight);
+		System.out.println("rectangle : perimetre = "+ rectangle.perimeter() + " aire = " + rectangle.area());
 		return null;
 	}
 
 	static ConvexPolygon buildSquare(Point upperLeft, int sideLength) {
 		Square square = new Square(upperLeft, sideLength);
+		System.out.println("carré : perimetre = "+ square.perimeter() + " aire = " + square.area());
 		return null;
 	}
 
 	static Ellipsis buildEllipsis(Point center, int xAxisLength, int yAxisLength) {
 		Ellipsis ellipsis = new Ellipsis(center, xAxisLength, yAxisLength);
+		System.out.println("ellipse : perimetre = "+ ellipsis.perimeter() + " aire = " + ellipsis.area());
 		return null;
 	}
 
 	static Ellipsis buildCircle(Point center, int radius) {
 		Circle circle = new Circle(center, radius);
+		System.out.println("cercle : perimetre = "+ circle.perimeter() + " aire = " + circle.area());
 		return null;
 	}
 
