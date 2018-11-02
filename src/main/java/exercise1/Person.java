9b77aa0ce19544b02e56d2b40df73b474984f3d6
PTom8 <171567@umons.ac.be>
2018-11-02 13:35:17 +0100

    mehdi aime la tajine au chocolat

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -3,4 +3,18 @@ package exercise1;
 class Person {
 	private String nom;
 	private Integer age;
+
+	public Person(String n, int a) {
+		nom = n;
+		age = a;
+		System.out.println("Veillez accueillir un nouveau né !!!! " + nom + " : " + age);
+	}
+
+	public String getName(){
+		return nom;
+	}
+
+	public int getAge (){
+		return age;
+	}
 }
