45a5fc025c0aa00a9340a9dcc58de6931854005a
Zyvrox <mehdi.zarioh@gmail.com>
2018-11-02 13:48:06 +0100

    Exo 2 : résolu

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,13 +1,17 @@
 package exercise2;
 
+import jdk.management.resource.internal.TotalResourceContext;
+
 class Person {
 	private int age;
+	private static int TotalAge=0;
 	private static int PopulationSize=0;
 
 
 	public Person(int a) {
 		age = a;
 		PopulationSize++;
+		TotalAge+=age;
 	}
 
 
@@ -15,4 +19,16 @@ class Person {
 	{
 		return PopulationSize;
 	}
+
+	public static float computeAveragePopulationAge()
+	{
+		return (float) TotalAge/PopulationSize;
+	}
+
+	public static void resetPopulation()
+	{
+		TotalAge=0;
+		PopulationSize=0;
+	}
+
 }
